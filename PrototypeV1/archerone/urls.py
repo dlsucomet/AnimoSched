5464f012b6eb8651b71c916ad57f446109ae9ed3
miguelllamas <34238731+miguelllamas@users.noreply.github.com>
2020-02-09 17:24:33 +0800

    Initial views / CSS mumbo-jumbo

--- PrototypeV1/archerone/urls.py
+++ PrototypeV1/archerone/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.index, name='login'),
+    path('signup/', views.index, name='signup'),
+    # path('profile/', views.index, name='profile'),
 ]
