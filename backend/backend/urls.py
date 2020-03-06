eb6edfd31fbd015855e21ce0e99eacadd2e7836e
1001-1100 <joshuacapulemanzano@gmail.com>
2020-03-06 22:01:34 +0800

    revamped to accomodate separate ReactJs and Django

---
+++ backend/backend/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include                 
+from rest_framework import routers                    
+from api import views                            
+
+router = routers.DefaultRouter()                      
+router.register(r'apis', views.TodoView, 'api')     
+
+urlpatterns = [
+    path('admin/', admin.site.urls), path('api/', include(router.urls))                
+]
