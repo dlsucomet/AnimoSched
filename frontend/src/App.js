c00e64c988b4ab74e8ac4e78d60e08e7593a3e45
Shenn Tinsay <shenntinsay@Macbokbokbitch.engenius.login>
2020-03-10 02:37:36 +0800

    landing page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,145 +1,79 @@
 // frontend/src/App.js
 
 import React, { Component } from "react";
-import Modal from "./components/Modal";
-import axios from "axios";
+import { Column, Row } from 'simple-flexbox';
+import Menu from './components/Menu';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewCompleted: false,
-      activeItem: {
-        title: "",
-        description: "",
-        completed: false
-      },
-      todoList: []
-    };
-  }
-  componentDidMount() {
-    this.refreshList();
-  }
-  refreshList = () => {
-    axios
-      .get("http://localhost:8000/api/apis/")
-      .then(res => this.setState({ todoList: res.data }))
-      .catch(err => console.log(err));
-  };
-  displayCompleted = status => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    }
-    return this.setState({ viewCompleted: false });
-  };
-  renderTabList = () => {
-    return (
-      <div className="my-5 tab-list">
-        <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "active" : ""}
-        >
-          complete
-        </span>
-        <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "" : "active"}
-        >
-          Incomplete
-        </span>
-      </div>
-    );
-  };
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      item => item.completed === viewCompleted
-    );
-    return newItems.map(item => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
-          title={item.description}
-        >
-          {item.title}
-        </span>
-        <span>
-          <button
-            onClick={() => this.editItem(item)}
-            className="btn btn-secondary mr-2"
-          >
-            {" "}
-            Edit{" "}
-          </button>
-          <button
-            onClick={() => this.handleDelete(item)}
-            className="btn btn-danger"
-          >
-            Delete{" "}
-          </button>
-        </span>
-      </li>
-    ));
-  };
-  toggle = () => {
-    this.setState({ modal: !this.state.modal });
-  };
-  handleSubmit = item => {
-    this.toggle();
-    if (item.id) {
-      axios
-        .put(`http://localhost:8000/api/apis/${item.id}/`, item)
-        .then(res => this.refreshList());
-      return;
-    }
-    axios
-      .post("http://localhost:8000/api/apis/", item)
-      .then(res => this.refreshList());
-  };
-  handleDelete = item => {
-    axios
-      .delete(`http://localhost:8000/api/apis/${item.id}`)
-      .then(res => this.refreshList());
-  };
-  createItem = () => {
-    const item = { title: "", description: "", completed: false };
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-  editItem = item => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
   render() {
     return (
-      <main className="content">
-        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-        <div className="row ">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="">
-                <button onClick={this.createItem} className="btn btn-primary">
-                  Add task
-                </button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush">
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
-        </div>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
-      </main>
+        <div>
+          <Menu />
+
+          <Column flexGrow={1}>
+
+          <Row vertical='center'>
+            <Column flexGrow={1}>
+              <Row horizontal='center'>
+                <h1>Create your schedule!</h1>
+              </Row>
+
+              <Row>
+                <Column flexGrow={1}>
+                  <h3> Icon 1 </h3>
+                </Column>
+
+                <Column flexGrow={1}>
+                  <span> Enter your <b>courses</b>,</span>
+                  <span> set your <b>preferences</b>, </span>
+                  <span> and choose a <b>schedule</b>, </span>
+                  <span> generated automatically. </span>
+                </Column>
+              </Row>
+
+              <br></br>
+
+              <Row>
+                <Column flexGrow={1}>
+                  <h3> Icon 2 </h3>
+                </Column>
+
+                <Column flexGrow={1}>
+                  <span> <b>Customize</b> the look </span>
+                  <span> of your schedule and </span>
+                  <span> save it as an image. </span>
+                </Column>
+              </Row>
+
+              <br></br>
+              
+              <Row>
+                <Column flexGrow={1}>
+                  <h3> Icon 3 </h3>
+                </Column>
+
+                {/* CHANGE THIS DESCRIPTION SINCE WE CHANGED THIS FUNCTIONALITY. */}
+                <Column flexGrow={1}>
+                  <span> <b>Collaborate</b> with friends </span>
+                  <span> and create schedules </span>
+                  <span> as a group. </span>
+                </Column>
+              </Row>
+            </Column>
+            
+            <Column flexGrow={1} horizontal='center'>
+              <button type="button" class="btn btn-success">Create Schedule</button>
+              <br/>
+              OR
+              {/* IS IT ADVISEABLE TO USE BREAK POINTS */}
+              <br/>
+              <br/>
+              <button type="button" class="btn btn-success">Check Flowchart</button>
+            </Column>
+          </Row>
+        </Column>
+      </div>        
     );
   }
 }
