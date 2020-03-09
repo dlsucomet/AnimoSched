69f312340cb9470adc5ce5495e78be1faf29a98a
1001-1100 <joshuacapulemanzano@gmail.com>
2020-03-09 18:04:58 +0800

    adding scripts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-8_1$5r2g!uxf8x26^oi9399#rp82xk0*$3)if+x=nte9aryd6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'api'
+    'api',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include                 
 from rest_framework import routers                    
 from api import views                            
+from core.views import index
 
 router = routers.DefaultRouter()                      
 router.register(r'apis', views.TodoView, 'api')     
 
 urlpatterns = [
-    path('admin/', admin.site.urls), path('api/', include(router.urls))                
+    path('', index, name='index'),
+    path('admin/', admin.site.urls), 
+    path('api/', include(router.urls)),                
 ]
