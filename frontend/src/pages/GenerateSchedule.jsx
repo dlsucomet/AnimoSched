1f2a8033115697b2e42b5aa7f9c54207b743d730
Adrienne Soliven <adriennesoliven@yahoo.com>
2020-03-27 22:39:06 +0800

    trying edit title and fixing homepage

--- frontend/src/pages/GenerateSchedule.jsx
+++ frontend/src/pages/GenerateSchedule.jsx
@@ -147,18 +147,18 @@ class GenerateSchedule extends Component {
         if(this.state.savedScheds.includes(this.state.generatedContents[index])){
             console.log("Saved Scheds: " + this.state.savedScheds.length);
             this.setState({saveButtonLabel: "Saved"});
-            const styleChange = {margin: "30px", backgroundColor: "white", color: "#16775D", borderStyle: "solid", borderColor: "#16775D"};
+            const styleChange = {margin: "30px", backgroundColor: "white", color: "#16775D"};
             this.setState({saveButtonStyle: styleChange});
         }else{
             this.setState({saveButtonLabel: "Save Schedule"});
-            const styleChange = {margin: "30px", backgroundColor: "#16775D", color: "white", border: "none"};
+            const styleChange = {margin: "30px", backgroundColor: "#16775D", color: "white"};
             this.setState({saveButtonStyle: styleChange});
         }
     }
 
     createSchedInfo = (arrayGenSched)=>{
         var generatedContents = arrayGenSched.map((item, index) =>
-                <GenSchedInfo key={item.id} id={item.id} scheduleContent={item.scheduleContent} tableContent={ item.tableContent} prefContent={item.prefContent} conflictsContent={item.conflictsContent}/>
+                <GenSchedInfo key={item.id} id={item.id} scheduleContent={item.scheduleContent} tableContent={ item.tableContent} prefContent={item.prefContent} conflictsContent={item.conflictsContent} titleName={item.title}/>
 
         );
 
@@ -242,6 +242,7 @@ class GenerateSchedule extends Component {
         var jsonSample =[
             {
                 id: 1,
+                title: "Schedule 1",
                 scheduleContent: [
                     {
                       title: "HUMAART",
@@ -301,6 +302,7 @@ class GenerateSchedule extends Component {
 
             {
                 id: 2,
+                title: "Schedule 2",
                 scheduleContent: [
                     {
                         title: "CSSERVM",
@@ -418,7 +420,7 @@ class GenerateSchedule extends Component {
                             </Row>
                         </div>
 
-                        <div className = "genSchedInfoContainer" style={style} ref={this.generatedRef} onChange={this.handleScrollToGen}>
+                        <div className = "genSchedInfoContainer" style={style} ref={this.generatedRef} >
                             <span>{this.state.currentContent}</span>
                         
                             <div className = "paginationContainer">
