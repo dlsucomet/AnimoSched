4628401d30530e7be10b985789b4d39124acab63
1001-1100 <joshuacapulemanzano@gmail.com>
2020-03-28 21:41:50 +0800

    add and remove courses in generate sched working locally

--- frontend/src/pages/GenerateSchedule.jsx
+++ frontend/src/pages/GenerateSchedule.jsx
@@ -40,6 +40,8 @@ class GenerateSchedule extends Component {
             savedScheds: [],
             saveButtonLabel: "Save Schedule",
             saveButtonStyle: {margin: "30px"},
+            //temp
+            id:0
         
 
             
@@ -134,35 +136,40 @@ class GenerateSchedule extends Component {
     //         console.log(this.state.highCourses)
     //     }
     // }
-    handleCourseDelete = (course_id) => {
+    handleCourseDelete = (addCourse) => {
+        console.log(addCourse)
+        console.log(this.state.courseList)
         const newCourseList = [];
         this.state.courseList.map(course => {
-            if(course.id != course_id){
-                console.log(course.course_code)
-                newCourseList.push(course)
-            }
+            newCourseList.push(course)
         })
+        newCourseList.push({'id':addCourse.course_id, 'course_code':addCourse.data})
+        console.log(newCourseList)
         this.setState({courseList:newCourseList})
     }
 
     handleAutoCompleteChange = (e, val) => {
         const newCourseList = [];
 
-        this.state.courseList.map(course => {
-            if(course.id != val.id){
-                console.log(course.course_code)
-                newCourseList.push(course)
+        if(val != undefined){
+            this.state.courseList.map(course => {
+                if(course.id != val.id){
+                    console.log(course.course_code)
+                    newCourseList.push(course)
+                }
+            })
+            this.setState({courseList:newCourseList})
+            if(val.course_code != undefined && val.course_code.trim() != ''){
+                this.state.id = this.state.id + 1;
+                const newCourse = {'id':this.state.id,'course_id':val.id,'data':val.course_code}; 
+                console.log(newCourse)
+                this.setState(state =>{
+                    const highCourses = state.highCourses.concat(newCourse);
+                    return{highCourses};
+                });
+                console.log(this.state.highCourses)
             }
-        })
-        this.setState({courseList:newCourseList})
-        if(val != undefined && val.course_code != undefined && val.course_code.trim() != ''){
-            const newCourse = {'id':0,'course_id':val.id,'data':val.course_code}; 
-            console.log(newCourse)
-            this.setState(state =>{
-                const highCourses = state.highCourses.concat(newCourse);
-                return{highCourses};
-            });
-            console.log(this.state.highCourses)
+            console.log(e.target)
         }
     }
     
@@ -446,12 +453,12 @@ class GenerateSchedule extends Component {
                                 <Row vertical = 'center'>
                                     <Column flexGrow={1} horizontal = 'center'>
                                         <h3 className='priortyTitle'>Highest Priority</h3>
-                                        <CourseDnD idTag={this.state.highPriorityId} courses={this.state.highCourses} updateFunction={this.updateHighPriorty}/>
+                                        <CourseDnD idTag={this.state.highPriorityId} courses={this.state.highCourses} updateFunction={this.updateHighPriorty} handleCourseDelete={this.handleCourseDelete}/>
 
                                     </Column>
                                     <Column flexGrow={1} horizontal = 'center'>
                                         <h3 className='priortyTitle'>Lowest Priority</h3>
-                                        <CourseDnD idTag={this.state.lowPriorityId} courses={this.state.lowCourses} updateFunction={this.updateLowPriority}/>
+                                        <CourseDnD idTag={this.state.lowPriorityId} courses={this.state.lowCourses} updateFunction={this.updateLowPriority} handleCourseDelete={this.handleCourseDelete}/>
                                     </Column>
                                 </Row>
                             </div>
