dbb2595828e19301428470bd46ef6afb2a4bfb06
Adrienne Soliven <adriennesoliven@yahoo.com>
2020-04-03 01:08:33 +0800

    All the profile fields have inline edit text

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -104,69 +104,70 @@ class Index extends Component {
             </Row>
            
           </div>
-  
-          {/* <Column flexGrow={1}>
-  
-            <Row vertical='center'>
-              <Column flexGrow={1}>
-                <Row horizontal='center'>
-                  <h1>Create your schedule!</h1>
-                </Row>
-  
-                <Row>
-                  <Column flexGrow={1}>
-                    <h3> Icon 1 </h3>
-                  </Column>
-  
-                  <Column flexGrow={1}>
-                    <span> Enter your <b>courses</b>,</span>
-                    <span> set your <b>preferences</b>, </span>
-                    <span> and choose a <b>schedule</b>, </span>
-                    <span> generated automatically. </span>
-                  </Column>
-                </Row>
-  
-                <br></br>
-  
-                <Row>
-                  <Column flexGrow={1}>
-                    <h3> Icon 2 </h3>
-                  </Column>
-  
-                  <Column flexGrow={1}>
-                    <span> <b>Customize</b> the look </span>
-                    <span> of your schedule and </span>
-                    <span> save it as an image. </span>
-                  </Column>
-                </Row>
-  
-                <br></br>
+          <div>
+            {/* <Column flexGrow={1}>
+    
+              <Row vertical='center'>
+                <Column flexGrow={1}>
+                  <Row horizontal='center'>
+                    <h1>Create your schedule!</h1>
+                  </Row>
+    
+                  <Row>
+                    <Column flexGrow={1}>
+                      <h3> Icon 1 </h3>
+                    </Column>
+    
+                    <Column flexGrow={1}>
+                      <span> Enter your <b>courses</b>,</span>
+                      <span> set your <b>preferences</b>, </span>
+                      <span> and choose a <b>schedule</b>, </span>
+                      <span> generated automatically. </span>
+                    </Column>
+                  </Row>
+    
+                  <br></br>
+    
+                  <Row>
+                    <Column flexGrow={1}>
+                      <h3> Icon 2 </h3>
+                    </Column>
+    
+                    <Column flexGrow={1}>
+                      <span> <b>Customize</b> the look </span>
+                      <span> of your schedule and </span>
+                      <span> save it as an image. </span>
+                    </Column>
+                  </Row>
+    
+                  <br></br>
+                  
+                  <Row>
+                    <Column flexGrow={1}>
+                      <h3> Icon 3 </h3>
+                    </Column>
+    
+                    {/* CHANGE THIS DESCRIPTION SINCE WE CHANGED THIS FUNCTIONALITY. */}
+                    {/* <Column flexGrow={1}>
+                      <span> <b>Collaborate</b> with friends </span>
+                      <span> and create schedules </span>
+                      <span> as a group. </span>
+                    </Column>
+                  </Row>
+                </Column> */}
                 
-                <Row>
-                  <Column flexGrow={1}>
-                    <h3> Icon 3 </h3>
-                  </Column>
-  
-                  {/* CHANGE THIS DESCRIPTION SINCE WE CHANGED THIS FUNCTIONALITY. */}
-                  {/* <Column flexGrow={1}>
-                    <span> <b>Collaborate</b> with friends </span>
-                    <span> and create schedules </span>
-                    <span> as a group. </span>
-                  </Column>
-                </Row>
-              </Column> */}
-              
-              {/* <Column flexGrow={1} horizontal='center'>
-                <button type="button" class="btn btn-success">Create Schedule</button>
-                <br/>
-                OR */}
-                {/* IS IT ADVISEABLE TO USE BREAK POINTS */}
-                {/* <br/>
-                <br/>
-                <button type="button" class="btn btn-success">Check Flowchart</button>
-              </Column>
-            </Row>
-          </Column> */}
+                {/* <Column flexGrow={1} horizontal='center'>
+                  <button type="button" class="btn btn-success">Create Schedule</button>
+                  <br/>
+                  OR */}
+                  {/* IS IT ADVISEABLE TO USE BREAK POINTS */}
+                  {/* <br/>
+                  <br/>
+                  <button type="button" class="btn btn-success">Check Flowchart</button>
+                </Column>
+              </Row>
+            </Column> */}
+          </div>
         </div>        
       );
     }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,6 +44,13 @@ const styles = theme => ({
     marginTop: -12,
     marginLeft: -12,
   },
+  backBtn:{
+    color: "white", 
+    marginLeft: "5px",
+  '&:hover': {
+      color: "#d3d3d3"
+    },
+  },
 });
 
 class Login extends Component {
@@ -181,7 +188,7 @@ class Login extends Component {
             <div class="sidenav">
                 <a className="backBtn" href="/">
                   <div className={"backBtn"}></div>
-                  <ArrowBackIosIcon fontSize="large" style={{color: "white", marginLeft: "5px"}} viewBox="0 0 1 24"/> <span className="backBtn">Back</span>
+                  <ArrowBackIosIcon fontSize="large"className={classes.backBtn} viewBox="0 0 1 24"/> <span className="backBtn">Back</span>
                     {/* <svg class="bi bi-backspace" width="3em" height="3em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8.603 4h7.08a1 1 0 011 1v10a1 1 0 01-1 1h-7.08a1 1 0 01-.76-.35L3 10l4.844-5.65A1 1 0 018.603 4zm7.08-1a2 2 0 012 2v10a2 2 0 01-2 2h-7.08a2 2 0 01-1.519-.698L2.241 10.65a1 1 0 010-1.302L7.084 3.7A2 2 0 018.603 3h7.08z" clip-rule="evenodd"></path>
                         <path fill-rule="evenodd" d="M7.83 7.146a.5.5 0 000 .708l5 5a.5.5 0 00.707-.708l-5-5a.5.5 0 00-.708 0z" clip-rule="evenodd"></path>
