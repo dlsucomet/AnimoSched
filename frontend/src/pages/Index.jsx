005d531e0ccdfd10a3169763842a5edce3f3daae
Adrienne Soliven <adriennesoliven@yahoo.com>
2020-04-06 22:49:50 +0800

    homepage changed layout & content can change w/ dummy data

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -28,11 +28,11 @@ import PropTypes from 'prop-types';
 const styles = theme => ({
   buttonStyle:{
       textTransform: "none",
-      width: "190px",
+      width: "150px",
       borderRadius: "25px",
       padding: "10px",
-      paddingLeft: "30px",
-      paddingRight: "30px",
+      paddingLeft: "10px",
+      paddingRight: "10px",
       backgroundColor: "#16775D",
       border: "none",
       color: "white",
@@ -43,6 +43,27 @@ const styles = theme => ({
       '&:hover': {
           backgroundColor: "white",
           color: "#16775D"
+        },
+  },
+
+    deleteButtonStyle:{
+      textTransform: "none",
+      width: "150px",
+      borderRadius: "25px",
+      padding: "10px",
+      paddingLeft: "10px",
+      paddingRight: "10px",
+      backgroundColor: "#D3D3D3",
+      border: "##D3D3D3",
+      color: "black",
+      boxShadow: "6px 5px #e8f4ea",
+      borderStyle: "solid",
+      borderColor: "#D3D3D3",
+      marginTop: "20px",
+      '&:hover': {
+          backgroundColor: "white",
+          borderStyle: "solid",
+          borderColor: "#D3D3D3",
         },
   }
 });
@@ -66,6 +87,157 @@ class Index extends Component {
       currentPage: 0,
       currentContent: <SchedViewHome/>,
       generatedContents: [<SchedViewHome/>,<SchedViewHome/>,<SchedViewHome/>],
+      pagesCount: 1,
+      schedules: [{
+          id: 1,
+          title: "Best Schedule",
+          scheduleContent: [
+            {
+                id: 3,
+                title: "CSSERVM",
+                section: "S15",
+                startDate: new Date(2018, 5, 26, 10, 0),
+                endDate: new Date(2018, 5, 26, 11, 0),
+                location: "G302",
+                professor: "Flowers, Fritz",
+                startTime: "09:30AM",
+                endTime: "11:30AM",
+                days: ['T', 'H'],
+                classCode: "2453"
+              },
+              {
+                title: "INOVATE",
+                section: "EB14",
+                startDate: new Date(2018, 5, 26, 12, 0),
+                endDate: new Date(2018, 5, 26, 13, 30),
+                id: 4,
+                location: "G305",
+                professor: "Tuazon, James Dean",
+                startTime: "12:00PM",
+                endTime: "01:30PM",
+                days: ['T', 'H'],
+                classCode: "2453"
+              },
+              {
+                  title: "HUMAART",
+                  section: "S17",
+                  startDate: new Date(2018, 5, 27, 9, 30),
+                  endDate: new Date(2018, 5, 27, 11, 30),
+                  id: 0,
+                  location: "G302",
+                  professor: "Sangi, April",
+                  startTime: "09:30AM",
+                  endTime: "11:30AM",
+                  days: ['M', 'W'],
+                  classCode: "2453"
+                  },
+          ],
+
+          tableContent:[
+            {
+              id: 3, 
+              classNmbr: '2345',
+              course: "DUMMY1", 
+              section:"S17", 
+              faculty: "DELA CRUZ, JUAN", 
+              day:"TH",
+              startTime: "08:00AM", 
+              endTime:"05:00PM",
+              room: "G310",
+              capacity: "40",
+              enrolled: "30"
+          },
+          {
+              id: 4, 
+              classNmbr: '2425',
+              course: "DUMMY2", 
+              section:"S15", 
+              faculty: "DEL TORRE, MARIA", 
+              day:"TH",
+              startTime: "08:00AM", 
+              endTime:"05:00PM",
+              room: "G310",
+              capacity: "40",
+              enrolled: "30"
+          }
+          ]
+        },
+        {
+          id: 2,
+          title: "Meh Schedule",
+          scheduleContent: [
+            {
+                id: 10,
+                title: "CSSERVM",
+                section: "S15",
+                startDate: new Date(2018, 5, 26, 13, 0),
+                endDate: new Date(2018, 5, 26, 14, 30),
+                location: "G302",
+                professor: "Flowers, Fritz",
+                startTime: "09:30AM",
+                endTime: "11:30AM",
+                days: ['T', 'H'],
+                classCode: "2453"
+              },
+              {
+                title: "INOVATE",
+                section: "EB14",
+                startDate: new Date(2018, 5, 26, 11, 0),
+                endDate: new Date(2018, 5, 26, 12, 30),
+                id: 11,
+                location: "G305",
+                professor: "Tuazon, James Dean",
+                startTime: "12:00PM",
+                endTime: "01:30PM",
+                days: ['T', 'H'],
+                classCode: "2453"
+              },
+              {
+                  title: "HUMAART",
+                  section: "S17",
+                  startDate: new Date(2018, 5, 27, 16, 30),
+                  endDate: new Date(2018, 5, 27, 18, 30),
+                  id: 12,
+                  location: "G302",
+                  professor: "Sangi, April",
+                  startTime: "09:30AM",
+                  endTime: "11:30AM",
+                  days: ['M', 'W'],
+                  classCode: "2453"
+                  },
+          ],
+
+          tableContent:[
+            {
+              id: 3, 
+              classNmbr: '2345',
+              course: "CSSERVM", 
+              section:"S17", 
+              faculty: "DELA CRUZ, JUAN", 
+              day:"TH",
+              startTime: "08:00AM", 
+              endTime:"05:00PM",
+              room: "G310",
+              capacity: "40",
+              enrolled: "30"
+          },
+          {
+              id: 4, 
+              classNmbr: '2425',
+              course: "HUMAART", 
+              section:"S15", 
+              faculty: "DEL TORRE, MARIA", 
+              day:"TH",
+              startTime: "08:00AM", 
+              endTime:"05:00PM",
+              room: "G310",
+              capacity: "40",
+              enrolled: "30"
+          }
+          ]
+        }
+      ]
+
       // generatedContents: [],
     }
 
@@ -85,6 +257,40 @@ class Index extends Component {
       console.log(this.state.generatedContents[index]);
   }
 
+  componentWillMount(){
+    this.setSchedInfo();
+  }
+
+  setSchedInfo = () => {
+    console.log(this.state.schedules)
+    var generatedContents = this.state.schedules.map((item, index) =>
+        <SchedViewHome key={item.id} id={item.id} /*offerings={item.offerings}*/ tableContent={item.tableContent} scheduleContent={item.scheduleContent} titleName={item.title} updateSchedTitle={this.updateSchedTitle}/>
+    );
+    this.setState({currentPage: 0})
+    this.setState({generatedContents});
+    // this.setState({hideGenContent: false});
+    this.setState({pagesCount: generatedContents.length});
+    this.setState({currentContent: generatedContents[0]})
+
+    }
+
+    updateSchedTitle=(text)=>{
+      var newArray = [];
+      const currentContent = this.state.currentContent;
+     // var index = newArray.findIndex(this.state.currentContent);
+     const newContent = <SchedViewHome key={currentContent.props.id} id={currentContent.props.id} scheduleContent={currentContent.props.scheduleContent} tableContent={currentContent.props.tableContent} prefContent={currentContent.props.prefContent} conflictsContent={currentContent.props.conflictsContent} titleName={text} updateSchedTitle={this.updateSchedTitle}/>
+
+     this.state.generatedContents.map(value=>{
+         if(value.key == this.state.currentContent.key){
+             newArray.push(newContent)
+         }else{
+             newArray.push(value)
+         }
+     })
+
+     this.setState({generatedContents: newArray});
+ }
+
     render() {
         this.state.pagesCount = this.state.generatedContents.length;
         this.state.currentContent = this.state.generatedContents[this.state.currentPage];
@@ -97,31 +303,31 @@ class Index extends Component {
 
           <div className={"homepage"} style={this.props.logged_in ? {} : {display: "none"}}>
             <div className={"hasContent"} style={(this.state.generatedContents.length > 0) ? {} : {display: "none"}}>
-              <Row horizontal="center">
-                  <div style={(this.state.generatedContents != null) ? {paddingTop:"20px"} : {display: "none"}}>
-                    <center ><h3 >FIRST TRIMESTER, AY 2019 - 2020</h3></center>
-                  </div>
-                </Row>
-              <Row horizontal="center">
-               
-                <Column flexShrink={1} >
-                  <div class="sidemenu" >
-                    <div style={(this.state.generatedContents != null) ? {} : {display: "none"}}>
-                      <center><input type="submit" class="btn btn-success change-term-sched" value="AY1920 T1" /></center>
-                      <center><input type="submit" class="btn btn-success change-term-sched" value="AY1819 T3" /></center>
-                    </div>
-                  </div>
-                </Column>
 
+              
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <br></br>
+                    <Typography gutterBottom variant="h3" align="center">
+                      FIRST TRIMESTER, AY 2019 - 2020
+                    </Typography>
+              </Grid>
+
+              <Grid item xs={2}>
+              </Grid>
+
+              <Grid item xs={7}>
                 <div className='savedContent'>
-                  <Column flexGrow={1}>
                     <span>{this.state.currentContent}</span>
-                  </Column>
                 </div>
+              </Grid>
 
-                <Column >
+              {/* <Grid item xs={1}>
+              </Grid> */}
+
+              <Grid item xs={3} align="center">
                 <div class='optionList'>
-                  {/* <Button
+                  <Button
                       variant="contained"
                       className={classes.buttonStyle}
                       >
@@ -141,14 +347,92 @@ class Index extends Component {
                   </Button>
                   <Button
                     variant="contained"
-                    className={classes.buttonStyle}
+                    className={classes.deleteButtonStyle}
                     >
                     Delete
-                  </Button> */}
-                    {/* <center><Button color="success" className="option-choices">EDIT</Button></center>
-                    <center> <Button color="success" className="option-choices">CUSTOMIZE</Button></center>
-                    <center><Button color="success" className="option-choices">EXPORT</Button></center>
-                    <center><Button color="secondary" className="option-choices">DELETE</Button></center> */}
+                  </Button>
+                  </div>
+              </Grid>
+
+              <Grid item xs={12} justify="center" alignItems="center">
+              <Row horizontal='center'>
+              <div className = "paginationContainer" style={(this.state.generatedContents != null) ? {} : {display: "none"}}>
+                
+                    <Pagination aria-label="Page navigation example">
+                        <PaginationItem disabled={this.state.currentPage <= 0}>
+                            <PaginationLink onClick={e => this.handlePageChange(e, this.state.currentPage - 1)}
+                                previous/>
+                        </PaginationItem>
+                        {[...Array(this.state.pagesCount)].map((page, i) => 
+                            <PaginationItem active={i === this.state.currentPage} key={i} className={'paginationItemStyle'}>
+                                <PaginationLink onClick={e => this.handlePageChange(e, i)} className={'paginationLinkStyle'}>
+                                {i + 1}
+                                </PaginationLink>
+                            </PaginationItem>
+                            )}
+                        <PaginationItem disabled={this.state.currentPage >= this.state.generatedContents.length - 1}>
+                            <PaginationLink
+                                onClick={e => this.handlePageChange(e, this.state.currentPage + 1)}
+                                next
+                            />
+                            
+                            </PaginationItem>
+                    </Pagination>
+                
+              </div>
+              </Row>
+              </Grid>
+
+            </Grid>
+              
+              {/* <Row horizontal="center">
+                  <div style={(this.state.generatedContents != null) ? {paddingTop:"20px"} : {display: "none"}}>
+                    <center ><h3 >FIRST TRIMESTER, AY 2019 - 2020</h3></center>
+                  </div>
+                </Row>
+              <Row horizontal="center"> */}
+               
+                {/* <Column flexShrink={1} >
+                  <div class="sidemenu" >
+                    <div style={(this.state.generatedContents != null) ? {} : {display: "none"}}>
+                      <center><input type="submit" class="btn btn-success change-term-sched" value="AY1920 T1" /></center>
+                      <center><input type="submit" class="btn btn-success change-term-sched" value="AY1819 T3" /></center>
+                    </div>
+                  </div>
+                </Column> */}
+
+                {/* <div className='savedContent'>
+                  <Column flexGrow={1}>
+                    <span>{this.state.currentContent}</span>
+                  </Column>
+                </div>
+
+                <Column >
+                <div class='optionList'>
+                  <Button
+                      variant="contained"
+                      className={classes.buttonStyle}
+                      >
+                      Edit
+                    </Button>
+                    <Button
+                    variant="contained"
+                    className={classes.buttonStyle}
+                    >
+                    Customize
+                  </Button>
+                  <Button
+                    variant="contained"
+                    className={classes.buttonStyle}
+                    >
+                    Export
+                  </Button>
+                  <Button
+                    variant="contained"
+                    className={classes.deleteButtonStyle}
+                    >
+                    Delete
+                  </Button>
                   </div>
                 </Column>
 
@@ -179,8 +463,8 @@ class Index extends Component {
                     </Pagination>
                 </Row>
               </div>
-            </Row>
-            </div>
+            </Row>*/}
+            </div> 
                           
                         
             <div className={"hasNoContent"} style={(this.state.generatedContents.length <= 0) ? {} : {display: "none"}}>
@@ -297,67 +581,6 @@ class Index extends Component {
             <Grid item xs={1}>
             </Grid>
           </Grid>
-
-            {/* <Column flexGrow={1}>
-              <Row vertical='center'>
-                <Column flexGrow={1}>
-                  <Row horizontal='center'>
-                    <h1>Create your schedule!</h1>
-                  </Row>
-    
-                  <Row>
-                    <Column flexGrow={1}>
-                      <h3> Icon 1 </h3>
-                    </Column>
-    
-                    <Column flexGrow={1}>
-                      <span> Enter your <b>courses</b>,</span>
-                      <span> set your <b>preferences</b>, </span>
-                      <span> and choose a <b>schedule</b>, </span>
-                      <span> generated automatically. </span>
-                    </Column>
-                  </Row>
-    
-                  <br></br>
-    
-                  <Row>
-                    <Column flexGrow={1}>
-                      <h3> Icon 2 </h3>
-                    </Column>
-    
-                    <Column flexGrow={1}>
-                      <span> <b>Customize</b> the look </span>
-                      <span> of your schedule and </span>
-                      <span> save it as an image. </span>
-                    </Column>
-                  </Row>
-    
-                  <br></br>
-                  
-                  <Row>
-                    <Column flexGrow={1}>
-                      <h3> Icon 3 </h3>
-                    </Column> */}
-    
-                    {/* CHANGE THIS DESCRIPTION SINCE WE CHANGED THIS FUNCTIONALITY. */}
-                    {/* <Column flexGrow={1}>
-                      <span> <b>Collaborate</b> with friends </span>
-                      <span> and create schedules </span>
-                      <span> as a group. </span>
-                    </Column>
-                  </Row>
-                </Column>                 
-                 <Column flexGrow={1} horizontal='center'>
-                  <button type="button" class="btn btn-success">Create Schedule</button>
-                  <br/>
-                  OR */}
-                  {/* IS IT ADVISEABLE TO USE BREAK POINTS */}
-                  {/* <br/>
-                  <br/>
-                  <button type="button" class="btn btn-success">Check Flowchart</button>
-                </Column>
-              </Row>
-            </Column> */}
           </div>
         </div>        
       );
@@ -365,8 +588,7 @@ class Index extends Component {
   }
 
   Index.propTypes={
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
   };
-
 
 export default withStyles(styles)(Index);
