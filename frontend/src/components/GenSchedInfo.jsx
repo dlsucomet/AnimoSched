613d8fd731c0f0dd5ae80db7375f5f10806e7fa2
Shenn Tinsay <shenntinsay@Macbok-bok-bitch.local>
2020-07-28 23:32:44 +0800

    table template

--- frontend/src/components/GenSchedInfo.jsx
+++ frontend/src/components/GenSchedInfo.jsx
@@ -5,13 +5,52 @@ import ClassesTable from '../components/ClassesTable';
 import BoxInfo from '../components/BoxInfo';
 import { Button } from 'reactstrap';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 import EditableLabel from 'react-inline-editing';
 
 import EditIcon from '@material-ui/icons/Edit';
 import DoneIcon from '@material-ui/icons/Done';
 
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: '#006A4E',
+        color: theme.palette.common.white,
+      },
+      body: {
+        fontSize: 14,
+        borderBottom: "1px solid white",
+      },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+function createData(name) {
+    return { name };
+}
+
+const rows = [
+    createData('Frozen yoghurt'),
+    createData('Ice cream sandwich'),
+    createData('Eclair'),
+    createData('Cupcake'),
+    createData('Gingerbread'),
+];
 
 const styles = theme => ({
     pencilIcon:{ 
@@ -135,6 +174,26 @@ class GenSchedInfo extends Component {
                                 
                                 <BoxInfo content={this.state.prefContent } id={1+this.state.id}/>
     
+                                <TableContainer component={Paper}>
+                                    <Table className={classes.table} aria-label="customized table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <StyledTableCell>Preferences</StyledTableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        
+                                        <TableBody>
+                                            {rows.map((row) => (
+                                                <StyledTableRow key={row.name}>
+                                                <StyledTableCell component="th" scope="row">
+                                                    {row.name}
+                                                </StyledTableCell>
+                                                </StyledTableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+
                             </Column>
                             <Column horizontal = 'center' style={{marginLeft: "20px"}}>
                                 Course Conflict
