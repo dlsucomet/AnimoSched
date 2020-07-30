902a6386b3e7a6b63c1ab618857e0058e726060f
Adrienne Soliven <adriennesoliven@yahoo.com>
2020-07-30 22:59:41 +0800

    centered

--- frontend/src/pages/SearchCourses.jsx
+++ frontend/src/pages/SearchCourses.jsx
@@ -408,9 +408,9 @@ class SearchCourses extends Component {
             <TableBody>
               <StyledTableRow>
                 <StyledTableCell colSpan={9}>
-                  <p>
+                  <center><p>
                     No available course offerings.
-                  </p>
+                  </p></center>
                 </StyledTableCell>
               </StyledTableRow>
             </TableBody>
