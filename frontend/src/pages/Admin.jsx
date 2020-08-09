f9fd02b0858be4188f4280cc3b1b736d47b1bbc1
1001-1100 <joshuacapulemanzano@gmail.com>
2020-08-09 15:23:28 +0800

    admin interface

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -9,12 +9,17 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class Admin extends Component {
     constructor(props){
         super(props)
         this.state = {
             rows: [],
+            enlists: [],
+            addIdnum: '',
             dataReceived: false
         }
     }
@@ -23,32 +28,100 @@ class Admin extends Component {
         return { course, section, classnumber, idnum, enrolled, capacity};
     }
 
+    createEnlistData(id, idnum, allowed){
+        return { id, idnum, allowed};
+    }
+
     componentDidMount(){
         this.setState({dataReceived: false})
         const rows = []
+        const enlists = []
         axios.get('https://archerone-backend.herokuapp.com/api/carts/').then(res => {
             res.data.map(cart => {
                 axios.get('https://archerone-backend.herokuapp.com/api/getclass/'+cart.classnumber).then(res => {
                     var data = res.data[0][0]
-                    console.log(res.data)
                     if(data != undefined){
                         rows.push(this.createData(data.course, data.section, data.classnumber, cart.idnum, data.current_enrolled, data.max_enrolled))
                         this.setState({rows, dataReceived: true})
-                        console.log(rows)
                     }
                 })
             })
         })
+        axios.get('https://archerone-backend.herokuapp.com/api/enlists/').then(res => {
+            console.log(res.data)
+            res.data.map(enlist => {
+                enlists.push(this.createEnlistData(enlist.id, enlist.idnum, enlist.allowed))
+                this.setState({enlists})
+            })
+        })
     }
 
+    handleSimul = () => {
+        axios.get('https://archerone-backend.herokuapp.com/api/enlist/').then(res => {
+            window.location.reload()
+        })
+    }
+
+    handleEmptyAll = () => {
+        axios.get('https://archerone-backend.herokuapp.com/api/empty/').then(res => {
+            window.location.reload()
+        })
+    }
+
+    handleFull = (classnumber) => {
+        axios.post('https://archerone-backend.herokuapp.com/api/modifyoffering/', {
+            classnumber,
+            action: 'full'
+        }).then(res => {
+            window.location.reload()
+        })
+    }
+
+    handleEmpty = (classnumber) => {
+        axios.post('https://archerone-backend.herokuapp.com/api/modifyoffering/', {
+            classnumber,
+            action: 'empty'
+        }).then(res => {
+            window.location.reload()
+        })
+    }
+
+    handleAllow = (id) => {
+        axios.post('https://archerone-backend.herokuapp.com/api/enlists/', {
+            idnum: id,
+            allowed: true
+        }).then(res => {
+            window.location.reload()
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({addIdnum: e.target.value})
+    }
+
+    handleAddEnlist = () => {
+        axios.post('https://archerone-backend.herokuapp.com/api/enlists/', {
+            idnum: this.state.addIdnum,
+            allowed: true
+        }).then(res => {
+            window.location.reload()
+        })
+    }
+    handleDeleteEnlist = (id) => {
+        axios.delete('https://archerone-backend.herokuapp.com/api/enlists/'+id).then(res => {
+            window.location.reload()
+        })
+    }
     render() {
       return (
         <div>
             {this.state.dataReceived ?
+            <div>
             <TableContainer component={Paper}>
             <Table aria-label="simple table">
                 <TableHead>
                 <TableRow>
+                    <TableCell>Action</TableCell>
                     <TableCell>Course</TableCell>
                     <TableCell align="right">Classnumber</TableCell>
                     <TableCell align="right">ID Number</TableCell>
@@ -59,6 +132,9 @@ class Admin extends Component {
                 <TableBody>
                 {this.state.rows.map((row) => (
                     <TableRow key={row.course}>
+                    <TableCell><Button variant="contained" onClick={() => this.handleFull(row.classnumber)}>Full</Button>
+                    <Button variant="contained" onClick={() => this.handleEmpty(row.classnumber)}>Empty</Button>
+                    <Button variant="contained" onClick={() => this.handleAllow(row.idnum)}>Allow</Button></TableCell>
                     <TableCell component="th" scope="row">
                         {row.course + ' ' + row.section}
                     </TableCell>
@@ -71,6 +147,45 @@ class Admin extends Component {
                 </TableBody>
             </Table>
             </TableContainer>
+            <Divider>
+
+            </Divider>
+
+            <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Actions</TableCell>
+                    <TableCell>ID Number</TableCell>
+                    <TableCell>Allowed</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {this.state.enlists.map((row) => (
+                    <TableRow key={row.idnum}>
+                    <TableCell>
+                        <Button variant="contained" onClick={() => this.handleDeleteEnlist(row.id)}>Delete</Button>
+
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        {row.idnum}
+                    </TableCell>
+                    <TableCell>{row.allowed ? 'true' : 'false'}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+
+            <span>
+                <Button variant="contained" onClick={this.handleSimul}>Simulate Enlistment</Button>
+                <Button variant="contained" onClick={this.handleEmptyAll}>Empty course offerings</Button>
+            </span>
+            <span>
+                <TextField onChange={this.handleChange}></TextField>
+                <Button variant="contained" onClick={this.handleAddEnlist}>Add allowed</Button>
+            </span>
+            </div>
             :
             null}
 
